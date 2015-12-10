78b8dec3250483da73f101300337277b85bda54a
NoOmega <alekomissarov@gmail.com>
2015-12-11 01:31:18 +0300

    Create SLList.h

---
+++ Singly-Linked-List/SLList.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+namespace Komissarov_Alexey
+{
+	struct SLLData
+	{
+		double index;
+		SLLData *next;
+	};
+
+	class SLList
+	{
+	private:
+		SLLData *begin;
+	public:
+		SLList ();
+		void Add (const double);	//Добавляет элемент в порядке возрастания
+		void Print (void);
+		void FLRepack (void);		//Функция переупаковывает список (1->2->3->4 => 1->4->2->3)
+		~SLList ();
+	};
+}
