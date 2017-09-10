73068336bab51729a7469a2240d68318ef107b64
NoOmega <alekomissarov@gmail.com>
2017-09-10 22:36:15 +0300

    Update FormulaSolver.cpp

--- FormulaSolver/FormulaSolver.cpp
+++ FormulaSolver/FormulaSolver.cpp
@@ -2,9 +2,8 @@
 
 #include <iostream>
 #include <sstream>
-#include <stdexcept>
-#include <vector>
 #include <memory>
+#include <vector>
 
 
 /// Операторы
@@ -16,6 +15,7 @@ enum class Operator : std::uint8_t
     Division = 3
 };
 
+/// Вспомогательные функции парсинга
 namespace ParserUtils
 {
     static int GetOperatorPriority(Operator iOperator)
@@ -68,6 +68,13 @@ namespace ParserUtils
         return false;
     }
 
+    static std::string GetString(double iNumber)
+    {
+        std::ostringstream strs;
+        strs << iNumber;
+        return strs.str();
+    }
+
     static bool GetOperator(const char& iCharacter, Operator& oOperator)
     {
         switch (iCharacter)
@@ -76,6 +83,19 @@ namespace ParserUtils
             case '-': oOperator = Operator::Substraction; return true;
             case '*': oOperator = Operator::Multiplication; return true;
             case '/': oOperator = Operator::Division; return true;
+        }
+
+        return false;
+    }
+
+    static bool GetString(Operator iOperator, std::string& oString)
+    {
+        switch (iOperator)
+        {
+            case Operator::Addition: oString = "+"; return true;
+            case Operator::Substraction: oString = "-"; return true;
+            case Operator::Multiplication: oString = "*"; return true;
+            case Operator::Division: oString = "/"; return true;
         }
 
         return false;
@@ -110,11 +130,12 @@ namespace ParserUtils
     static bool IsOpeningBracketBeforeNextOperator(const std::string& iString, std::string::const_iterator iStart, bool isOpeningBracket = true)
     {
         auto bracket = isOpeningBracket ? '(' : ')';
+        auto opposite = isOpeningBracket ? ')' : '(';
         for (auto iter = iStart; iter != iString.end(); ++iter)
         {
             if (*iter == bracket)
                 return true;
-            else if (IsOperator(*iter))
+            else if (IsOperator(*iter) || *iter == opposite)
                 return false;
         }
 
@@ -170,6 +191,7 @@ namespace ParserUtils
     }
 }
 
+/// Лексическое дерево
 namespace Tree
 {
     typedef std::unique_ptr<class Node> NodePtr;
@@ -191,6 +213,8 @@ namespace Tree
         static NodePtr Create(Operator iOperator, NodePtr&& iFirst, NodePtr&& iSecond);
 
         virtual bool GetValue(double& oValue) const = 0;
+
+        virtual void PrintToConsole(std::vector<std::string>& ioLevels, size_t iLevel, std::vector<std::vector<size_t>>& ioVConnections) const = 0;
     };
 
     /// Узел с константой
@@ -201,6 +225,8 @@ namespace Tree
 
         virtual bool GetValue(double& oValue) const override;
 
+        virtual void PrintToConsole(std::vector<std::string>& ioLevels, size_t iLevel, std::vector<std::vector<size_t>>& ioVConnections) const override;
+
     private:
         double _value;
     };
@@ -209,13 +235,15 @@ namespace Tree
     class OperationNode : public Node
     {
     public:
-        OperationNode(Operator iOperator, NodePtr&& iFirst, NodePtr&& iSecond);
+        OperationNode(Operator iOperator, NodePtr&& iLeft, NodePtr&& iRight);
 
         virtual bool GetValue(double& oValue) const override;
 
+        virtual void PrintToConsole(std::vector<std::string>& ioLevels, size_t iLevel, std::vector<std::vector<size_t>>& ioVConnections) const override;
+
     private:
-        NodePtr _first;
-        NodePtr _second;
+        NodePtr _left;
+        NodePtr _right;
         Operator _operator;
     };
 
@@ -227,9 +255,9 @@ namespace Tree
         return NodePtr(new ConstantNode(iValue));
     }
 
-    NodePtr Node::Create(Operator iOperator, NodePtr&& iFirst, NodePtr&& iSecond)
+    NodePtr Node::Create(Operator iOperator, NodePtr&& iLeft, NodePtr&& iRight)
     {
-        return NodePtr(new OperationNode(iOperator, std::move(iFirst), std::move(iSecond)));
+        return NodePtr(new OperationNode(iOperator, std::move(iLeft), std::move(iRight)));
     }
 
     // CONSTATNT NODE
@@ -240,22 +268,31 @@ namespace Tree
         return true;
     }
 
+    void ConstantNode::PrintToConsole(std::vector<std::string>& ioLevels, size_t iLevel, std::vector<std::vector<size_t>>& ioVConnections) const
+    {
+        ioLevels[iLevel] += "(" + ParserUtils::GetString(_value) + ")";
+    }
+
     // OPERATION NODE
 
-    OperationNode::OperationNode(Operator iOperator, NodePtr&& iFirst, NodePtr&& iSecond)
+    const char vConnect = '|';
+    const std::string hConnect = "--";
+    const std::string emptyConsoleLine = "                                                                               ";
+
+    OperationNode::OperationNode(Operator iOperator, NodePtr&& iLeft, NodePtr&& iRight)
     {
-        _first = std::move(iFirst);
-        _second = std::move(iSecond);
+        _left = std::move(iLeft);
+        _right = std::move(iRight);
         _operator = iOperator;
     }
 
     bool OperationNode::GetValue(double& oValue) const
     {
-        if (_first == nullptr || _second == nullptr)
+        if (_left == nullptr || _right == nullptr)
             return false;
 
         double a = 0., b = 0.;
-        if (!_first->GetValue(a) || !_second->GetValue(b))
+        if (!_left->GetValue(a) || !_right->GetValue(b))
             return false;
 
         switch (_operator)
@@ -287,24 +324,100 @@ namespace Tree
 
         return false;
     }
+
+    void OperationNode::PrintToConsole(std::vector<std::string>& ioLevels, size_t iLevel, std::vector<std::vector<size_t>>& ioVConnections) const
+    {
+        auto startIndex = ioLevels[iLevel].size();
+
+        // Выводим оператор
+        std::string operatorStr;
+        if (!ParserUtils::GetString(_operator, operatorStr))
+            return;
+
+        ioLevels[iLevel] += "(" + operatorStr + ")";
+
+        // Правый операнд
+        ioLevels[iLevel] += hConnect;
+        _right->PrintToConsole(ioLevels, iLevel, ioVConnections);
+
+        // Вертикальная связь
+        for (auto i = iLevel; i < ioLevels.size(); ++i)
+        {
+            ioVConnections[i].push_back(startIndex + 1);
+        }
+
+        // Добавляем новый уровень
+        ioLevels.push_back("");
+        ioVConnections.push_back(std::vector<size_t>());
+
+        // Добавляем пробелы слева
+        for (decltype(startIndex) i = 0; i < startIndex; ++i)
+        {
+            ioLevels.back() += " ";
+        }
+
+        // Левый операнд
+        _left->PrintToConsole(ioLevels, ioLevels.size() - 1, ioVConnections);
+    }
+
+    /// TREE
+
+    void PrintToConsole(const NodePtr& iTopNode)
+    {
+        if (iTopNode == nullptr)
+        {
+            std::cout << std::endl << "Empty tree.";
+            return;
+        }
+
+        std::vector<std::string> levels;
+        levels.push_back("");
+        std::vector<std::vector<size_t>> vConnections;
+        vConnections.push_back(std::vector<size_t>());
+
+        iTopNode->PrintToConsole(levels, 0, vConnections);
+
+        std::vector<std::string> subLevels;
+        for (const auto& vConnLevel : vConnections)
+        {
+            subLevels.push_back(emptyConsoleLine);
+            for (const auto& vConnIndex : vConnLevel)
+            {
+                subLevels.back()[vConnIndex] = vConnect;
+                if (levels.at(subLevels.size() - 1).at(vConnIndex) == ' ')
+                    levels.at(subLevels.size() - 1).at(vConnIndex) = vConnect;
+            }
+        }
+
+        // Output
+        std::cout << std::endl;
+        for (size_t i = 0; i < levels.size(); ++i)
+        {
+            std::cout << levels.at(i) << std::endl << subLevels.at(i) << std::endl;
+        }
+    }
 }
 
-namespace Parser
+class Parser
 {
-    // Объявление функции
-    FormulaSolver::Result RecursiveParseOperation(Tree::NodePtr&&, const std::string&, std::string::const_iterator&, Tree::NodePtr&);
+    Parser();
 
-    FormulaSolver::Result RecursiveParseBlock(const std::string& iString, std::string::const_iterator& iBlockStartIter, Tree::NodePtr& oBlockNode)
+public:
+    static FormulaSolver::Result RecursiveParseBlock(const std::string& iString, std::string::const_iterator& iBlockStartIter, Tree::NodePtr& oBlockNode)
     {
         if (iBlockStartIter == iString.end())
             return FormulaSolver::Result::Success;
 
+        // Обработка отрицательного числа в начале блока
         double firstNum = 0.;
         bool firstNumIsNegative = false;
         if (ParserUtils::IsOperator(*iBlockStartIter))
         {
             if (*iBlockStartIter == '-')
+            {
                 firstNumIsNegative = true;
+                ++iBlockStartIter;
+            }
             else
                 return FormulaSolver::Result::BadOperand;
         }
@@ -319,6 +432,8 @@ namespace Parser
             // Если блок состоит только из вложенного - возвращаем результат
             if (iBlockStartIter == iString.end())
                 return res;
+
+            --iBlockStartIter;
         }
         else
         {
@@ -331,7 +446,7 @@ namespace Parser
 
             oBlockNode = Tree::Node::Create(firstNum);
 
-            // (-3), (2), ()
+            // Обработка блоков без операций (-3), (2), ()
             if (ParserUtils::IsClosingBracketBeforeNextOperator(iString, iBlockStartIter))
             {
                 ParserUtils::MoveIterNextAfterBlock(iString, iBlockStartIter);
@@ -339,6 +454,7 @@ namespace Parser
             }
         }
 
+        // Пока в блоке есть операторы - парсим их
         auto currentOperatorIter = ParserUtils::FindNextOperator(iString, iBlockStartIter);
         while (currentOperatorIter != iBlockStartIter)
         {
@@ -348,8 +464,9 @@ namespace Parser
 
             if (ParserUtils::IsClosingBracketBeforeNextOperator(iString, currentOperatorIter))
             {
+                // Конец блока
                 ParserUtils::MoveIterNextAfterBlock(iString, currentOperatorIter);
-                iBlockStartIter = currentOperatorIter - 1;
+                iBlockStartIter = currentOperatorIter;
                 return FormulaSolver::Result::Success;
             }
 
@@ -360,7 +477,7 @@ namespace Parser
         return FormulaSolver::Result::Success;
     }
 
-    FormulaSolver::Result RecursiveParseOperation(Tree::NodePtr&& iLeft, const std::string& iString, std::string::const_iterator& iOperatorIter, Tree::NodePtr& oOperationNode)
+    static FormulaSolver::Result RecursiveParseOperation(Tree::NodePtr&& iLeft, const std::string& iString, std::string::const_iterator& iOperatorIter, Tree::NodePtr& oOperationNode)
     {
         Operator currentOperator;
         if (!ParserUtils::GetOperator(*iOperatorIter, currentOperator))
@@ -369,6 +486,7 @@ namespace Parser
         Tree::NodePtr right = nullptr;
         if (ParserUtils::IsOpeningBracketBeforeNextOperator(iString, iOperatorIter + 1))
         {
+            // Если правый операнд - блок, парсим его
             auto blockIter = iOperatorIter + 1;
             ParserUtils::MoveIterIntoNextBlock(iString, blockIter);
 
@@ -378,6 +496,7 @@ namespace Parser
         }
         else
         {
+            // Берём правый операнд и создаём узел
             double num = 0.;
             if (!ParserUtils::GetRightOperand(iString, iOperatorIter, num))
                 return FormulaSolver::Result::BadOperand;
@@ -387,10 +506,12 @@ namespace Parser
 
         if (ParserUtils::IsClosingBracketBeforeNextOperator(iString, iOperatorIter + 1))
         {
+            // Если последняя операция в блоке
             ++iOperatorIter;
         }
         else
         {
+            // Находим следующий оператор
             auto nextOperatorIter = ParserUtils::FindNextOperator(iString, iOperatorIter);
             if (nextOperatorIter != iOperatorIter)
             {
@@ -398,7 +519,7 @@ namespace Parser
                 if (!ParserUtils::GetOperator(*nextOperatorIter, nextOperator))
                     return FormulaSolver::Result::UnknownOperator;
 
-                // Проверка приоритетов текущей и следующей операций
+                // Проверка приоритетов текущего и следующего оператора
                 auto currPriority = ParserUtils::GetOperatorPriority(currentOperator);
                 auto nextPriority = ParserUtils::GetOperatorPriority(nextOperator);
                 if (currPriority < 0 || nextPriority < 0)
@@ -419,19 +540,20 @@ namespace Parser
         oOperationNode = Tree::Node::Create(currentOperator, std::move(iLeft), std::move(right));
         return FormulaSolver::Result::Success;
     }
-}
+};
 
 // FORMULA SOLVER
 
-FormulaSolver::Result FormulaSolver::Solve(const std::string& iFormulaString, double& oResult)
+FormulaSolver::Result FormulaSolver::Solve(const std::string& iFormulaString, double& oResult, bool iShowTree)
 {
-    Tree::NodePtr top = nullptr;
-
-    // Убираем пробелы
+    // Убираем пробелы и проверяем скобки
     auto formula = iFormulaString;
     auto bracketStack = 0;
     for (auto it = formula.begin(); it != formula.end();)
     {
+        if (bracketStack < 0)
+            return Result::BadParentheses;
+
         if (*it == ' ')
         {
             it = formula.erase(it);
@@ -453,16 +575,22 @@ FormulaSolver::Result FormulaSolver::Solve(const std::string& iFormulaString, do
 
     if (bracketStack != 0)
         return Result::BadParentheses;
-
+    
+    // Парсим формулу и строим дерево
+    Tree::NodePtr top = nullptr;
     auto res = Parser::RecursiveParseBlock(formula, formula.begin(), top);
     if (res != Result::Success)
         return res;
 
     if (top == nullptr)
-        return Result::BadTree;
+        return Result::InternalError;
+
+    // Вывод дерева
+    if (iShowTree)
+        Tree::PrintToConsole(top);
 
     if (!top->GetValue(oResult))
-        return Result::BadTree;
+        return Result::InternalError;
 
     return Result::Success;
 }
@@ -477,8 +605,8 @@ void FormulaSolver::ResultToConsole(FormulaSolver::Result iResult)
         case Result::BadParentheses: std::cout << "Error in brackets"; break;
         case Result::EmptyInputString: std::cout << "Empty formula"; break;
         case Result::UnknownOperator: std::cout << "There is unknown operator"; break;
-        case Result::UnknownOperatorPriority: std::cout << "The creature that added new operator forgot to set priority in \"GetOperatorPriority()\""; break;
+        case Result::UnknownOperatorPriority: std::cout << "You forgot to set priority for new operator in \"GetOperatorPriority()\""; break;
         case Result::BadCharacter: std::cout << "Bad character"; break;
-        case Result::BadTree: std::cout << "Bad lexical tree"; break;
+        case Result::InternalError: std::cout << "Internal solver error, revision required"; break;
     }
 }
